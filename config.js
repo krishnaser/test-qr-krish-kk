3a5f678e2b69f1b02766bb3c9ffb26a8ae8df012
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-15 07:41:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://godzenitsu:safeer.123.@nezuko.7kvhs7q.mongodb.net/",
-  SESSION_NAME: process.env.SESSION_NAME || "QUEEN NEZUKO"
+  SESSION_NAME: process.env.SESSION_NAME || "oreobyshizo~"
 };
